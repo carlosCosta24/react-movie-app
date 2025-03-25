389fca42ada489a191fd45e3a44148fc17a0d407
carlosCosta <bodymgahed@yahoo.com>
2025-03-25 02:55:06 -0300

    nav main structure

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-
+import Nav from './components/navbar/navbar'
 function App() {
 
   return (
-    <>
+    <div class='flex' >
 
-    </>
+      <Nav />
+
+    </div>
   )
 }
 
---
+++ movie-app/src/components/navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+
+/*import themToggler from ''*/
+
+const Nav = () => {
+
+    return (
+        <nav>
+            <ul>
+                <li>movies </li>
+                <li>tv shows </li>
+
+            </ul>
+        </nav>
+
+
+
+    )
+}
+
+export default Nav
